b1e458558a5bb6781125b3bc6c88c39e7171f9ff
oleksandr <oleksandrlazebnij8@gmail.com>
2024-04-29 13:34:56 +0300

    found metrics using built in sklearn funcs

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,24 +5,11 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from sklearn.metrics import precision_recall_curve, auc, roc_curve
-
-# 1st. task
-df = pd.read_csv('KM-12-2.csv')
-# print(df.to_string())
-
-# =====================================================================================================
-
-# 2nd. task
-gt_counts = df['GT'].value_counts()
-
-print(f"Розподіл об'єктів відповідних класів: {gt_counts.to_string()}")
-print("Оскільки кількість об'єктів однакова для кожного класу — набір даних збалансований.")
+from sklearn.metrics import precision_recall_curve, auc, roc_curve, confusion_matrix, accuracy_score, precision_score, \
+    recall_score, f1_score, matthews_corrcoef, balanced_accuracy_score
 
 
-# =====================================================================================================
-
-# 3rd. task
+# ---------------------- func block ---------------------- #
 
 def get_x_values(start, step):
     x_values = []
@@ -37,6 +24,42 @@ def get_x_values(start, step):
         x_values.append(1.0)
 
     return x_values
+
+
+def get_metrics_for_pred(y_pred, y_actual, to_print=True):
+    confusion_matrix_test = confusion_matrix(y_actual, y_pred)
+    tn, fp, fn, tp = confusion_matrix_test.ravel()
+    sensitivity_test = tp / (tp + fn)
+    specificity_test = tn / (tn + fp)
+
+    accuracy = accuracy_score(y_actual, y_pred)
+    precision = precision_score(y_actual, y_pred, zero_division=1)
+    recall = recall_score(y_actual, y_pred)
+    f1_score_val = f1_score(y_actual, y_pred)
+    mcc = matthews_corrcoef(y_actual, y_pred)
+    balanced_accuracy = balanced_accuracy_score(y_actual, y_pred)
+    yodens_j = sensitivity_test + specificity_test - 1
+
+    if to_print:
+        print("Confusion Matrix:\n", confusion_matrix_test)
+        print("Accuracy:", accuracy)
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("F1 Score:", f1_score_val)
+        print("MCC: ", mcc)
+        print("Balanced accuracy: ", balanced_accuracy)
+        print("Youden's J statistics: ", yodens_j)
+
+    return {
+        "Accuracy": accuracy,
+        "Precision": precision,
+        "Recall": recall,
+        "F1 Score": f1_score_val,
+        "MCC": mcc,
+        "Balanced Accuracy": balanced_accuracy,
+        "Youden's J statistics": yodens_j,
+        "Confusion Matrix": list(confusion_matrix_test)
+    }
 
 
 def get_predictions(actual, probabilities, threshold_val):
@@ -56,210 +79,89 @@ def get_predictions(actual, probabilities, threshold_val):
     return predictions_df
 
 
-def get_accuracy(predictions, threshold):
-    accuracy = (predictions[threshold] == predictions['GT']).mean()
-    return accuracy
+# ---------------------- func block ---------------------- #
+
+# =====================================================================================================
 
 
-def get_confusion_map(predictions, threshold):
-    confusion_map = {
-        'TP': ((predictions[threshold] == 1) & (predictions['GT'] == 1)).sum(),
-        'FP': ((predictions[threshold] == 1) & (predictions['GT'] == 0)).sum(),
-        'FN': ((predictions[threshold] == 0) & (predictions['GT'] == 1)).sum(),
-        'TN': ((predictions[threshold] == 0) & (predictions['GT'] == 0)).sum()
-    }
+# 1st. task
+df = pd.read_csv('KM-12-2.csv')
+print(f'''
+1. Відкрити та зчитати дані з наданого файлу. Файл містить п’ять стовпчиків: \n{df.head()}\n
+a. Фактичне значення цільової характеристики. \n{df["GT"].head()}\n
+b. Результат передбачення моделі No1 у вигляді ймовірності
+приналежності об’єкту до класу 0. \n{df["Model_1_0"].head()}\n
+c. Результат передбачення моделі No1 у вигляді ймовірності
+приналежності об’єкту до класу 1. \n{df["Model_1_1"].head()}\n
+d. Результат передбачення моделі No2 у вигляді ймовірності
+приналежності об’єкту до класу 0. \n{df["Model_2_0"].head()}\n
+e. Результат передбачення моделі No2 у вигляді ймовірності
+приналежності об’єкту до класу 1. \n{df["Model_2_1"].head()}\n
+''')
 
-    return confusion_map
+# =====================================================================================================
 
+# 2nd. task
+print('''
+2. Визначити збалансованість набору даних. Вивести кількість об’єктів
+кожного класу.
+''')
 
-def get_precision(predictions, threshold):
-    confusion_map = get_confusion_map(predictions, threshold)
+gt_counts = df['GT'].value_counts()
 
-    precision = confusion_map['TP'] / (confusion_map['TP'] + confusion_map['FP'])
+print(f"Розподіл об'єктів відповідних класів: {gt_counts.to_string()}")
+print("Оскільки кількість об'єктів однакова для кожного класу — набір даних збалансований.\n")
 
-    return precision
+# =====================================================================================================
 
-
-def get_recall_1(predictions, threshold):
-    confusion_map = get_confusion_map(predictions, threshold)
-
-    recall = confusion_map['TP'] / (confusion_map['TP'] + confusion_map['FN'])
-
-    return recall
-
-
-def get_recall_0(predictions, threshold):
-    confusion_map = get_confusion_map(predictions, threshold)
-
-    recall = confusion_map['TN'] / (confusion_map['TN'] + confusion_map['FP'])
-
-    return recall
-
-
-def get_f_scores(precision, recall):
-    return 2 * precision * recall / (precision + recall)
-
-
-def get_MCC(predictions, threshold):
-    confusion_map = get_confusion_map(predictions, threshold)
-
-    numerator = confusion_map['TP'] * confusion_map['TN'] - confusion_map['FP'] * confusion_map['FN']
-    denominator = math.sqrt((confusion_map['TP'] + confusion_map['FP']) *
-                            (confusion_map['TP'] + confusion_map['FN']) *
-                            (confusion_map['TN'] + confusion_map['FP']) *
-                            (confusion_map['TN'] + confusion_map['FN'])
-                            )
-
-    if denominator == 0:
-        mcc = 0
-    else:
-        mcc = numerator / denominator
-
-    return mcc
-
-
-def get_sensitivity_specificity(predictions, threshold):
-    confusion_map = get_confusion_map(predictions, threshold)
-
-    sensitivity = confusion_map['TP'] / (confusion_map['TP'] + confusion_map['FN'])
-    specificity = confusion_map['TN'] / (confusion_map['TN'] + confusion_map['FP'])
-
-    return sensitivity, specificity
-
-
-def get_balanced_acc(predictions, threshold):
-    sensitivity, specificity = get_sensitivity_specificity(predictions, threshold)
-
-    balanced_accuracy = (sensitivity + specificity) / 2
-
-    return balanced_accuracy
-
-
-def get_youden_j(predictions, threshold):
-    sensitivity, specificity = get_sensitivity_specificity(predictions, threshold)
-
-    youden_j = sensitivity + specificity - 1
-
-    return youden_j
-
+# 3rd. task
+print("3. Для зчитаного набору даних виконати наступні дії:")
 
 step = 0.05
 start_threshold = 0
 
 x_values = get_x_values(start_threshold, step)
-print(x_values)
 
 # 3rd. task pt. a
-
 predictions_df_m1 = get_predictions(df['GT'], df['Model_1_1'], x_values)
 predictions_df_m2 = get_predictions(df['GT'], df['Model_2_1'], x_values)
 
-# print(predictions_df_m1.to_string())
+print(predictions_df_m1.head())
 
 # Accuracy
+metrics_m1 = list(map(lambda x: get_metrics_for_pred(predictions_df_m1[x], predictions_df_m1["GT"], False),
+                      map(str, x_values)))
 
-accuracy_m1 = list(map(lambda x: get_accuracy(predictions_df_m1, x),
-                       map(str, x_values)))
+metrics_m2 = list(map(lambda x: get_metrics_for_pred(predictions_df_m2[x], predictions_df_m2["GT"], False),
+                      map(str, x_values)))
 
-accuracy_m2 = list(map(lambda x: get_accuracy(predictions_df_m2, x),
-                       map(str, x_values)))
-
-print(f"Accuracy model 1: {accuracy_m1}")
-print(f"Accuracy model 2: {accuracy_m2}")
-
-# Precision
-
-
-precision_m1 = list(map(lambda x: get_precision(predictions_df_m1, x),
-                        map(str, x_values)))
-
-precision_m2 = list(map(lambda x: get_precision(predictions_df_m2, x),
-                        map(str, x_values)))
-
-print(f"Precision model 1: {precision_m1}")
-print(f"Precision model 2: {precision_m2}")
-
-# Recall
-recall_m1 = list(map(lambda x: get_recall_1(predictions_df_m1, x),
-                     map(str, x_values)))
-
-recall_m2 = list(map(lambda x: get_recall_1(predictions_df_m2, x),
-                     map(str, x_values)))
-
-print(f"Recall model 1: {recall_m1}")
-print(f"Recall model 2: {recall_m2}")
-
-# F-Scores
-f_scores_m1 = list(map(lambda x, y: get_f_scores(x, y),
-                       precision_m1, recall_m1))
-
-f_scores_m2 = list(map(lambda x, y: get_f_scores(x, y),
-                       precision_m2, recall_m2))
-
-print(f"F-Scores model 1: {f_scores_m1}")
-print(f"F-Scores model 2: {f_scores_m2}")
-
-# Matthew Correlation Coefficient
-mcc_m1 = list(map(lambda x: get_MCC(predictions_df_m1, x),
-                  map(str, x_values)))
-
-mcc_m2 = list(map(lambda x: get_MCC(predictions_df_m2, x),
-                  map(str, x_values)))
-
-print(f"Matthew Correlation Coefficient model 1: {mcc_m1}")
-print(f"Matthew Correlation Coefficient model 2: {mcc_m2}")
-
-# Balanced Accuracy
-balanced_acc_m1 = list(map(lambda x: get_balanced_acc(predictions_df_m1, x),
-                           map(str, x_values)))
-
-balanced_acc_m2 = list(map(lambda x: get_balanced_acc(predictions_df_m2, x),
-                           map(str, x_values)))
-
-print(f"Balanced accuracy model 1: {balanced_acc_m1}")
-print(f"Balanced accuracy model 2: {balanced_acc_m2}")
-
-# Youden J
-
-youden_j_m1 = list(map(lambda x: get_youden_j(predictions_df_m1, x),
-                       map(str, x_values)))
-
-youden_j_m2 = list(map(lambda x: get_youden_j(predictions_df_m2, x),
-                       map(str, x_values)))
-
-print(f"Youden's J Statistics model 1: {youden_j_m1}")
-print(f"Youden's J Statistics model 2: {youden_j_m2}")
+print(metrics_m1[2]["Accuracy"])
+print(metrics_m2[2]["Accuracy"])
 
 # Area under curve for Precision-Recall Curve
-
 precision_for_curve_m1, recall_for_curve_m1, thresholds_pr_m1 = precision_recall_curve(df['GT'], df['Model_1_1'])
 precision_for_curve_m2, recall_for_curve_m2, thresholds_pr_m2 = precision_recall_curve(df['GT'], df['Model_2_1'])
-
-print(len(thresholds_pr_m1))
-print(len(thresholds_pr_m2))
 
 auc_pr_m1 = auc(recall_for_curve_m1, precision_for_curve_m1)
 auc_pr_m2 = auc(recall_for_curve_m2, precision_for_curve_m2)
 
 print("Area Under Curve for Precision-Recall Curve model 1:", auc_pr_m1)
 print("Area Under Curve for Precision-Recall Curve model 2:", auc_pr_m2)
+print()
 
 # Area under curve for Receiver Operation Curve
-
-
 fpr_m1, tpr_m1, thresholds_roc_m1 = roc_curve(df['GT'], df['Model_1_1'])
 fpr_m2, tpr_m2, thresholds_roc_m2 = roc_curve(df['GT'], df['Model_2_1'])
-
-print(len(thresholds_roc_m1))
-print(len(thresholds_roc_m2))
 
 auc_roc_m1 = auc(fpr_m1, tpr_m1)
 auc_roc_m2 = auc(fpr_m2, tpr_m2)
 
 print("Area Under Curve for Receiver Operation Curve model 1:", auc_roc_m1)
 print("Area Under Curve for Receiver Operation Curve model 2:", auc_roc_m2)
+print()
 
 # 3rd. task pt. b
+print("\nb. Графіки. Metrics vs Threshold for Model 1/2.")
 
 colors = ['red', 'blue', 'green', 'orange', 'purple', 'cyan', 'magenta']
 
@@ -267,6 +169,15 @@ plt.figure(figsize=(10, 6))
 ticks = get_x_values(0, 0.1)
 plt.xticks(ticks)
 plt.yticks(ticks)
+
+accuracy_m1 = [metrics_for_thres['Accuracy'] for metrics_for_thres in metrics_m1]
+precision_m1 = [metrics_for_thres['Precision'] for metrics_for_thres in metrics_m1]
+recall_m1 = [metrics_for_thres['Recall'] for metrics_for_thres in metrics_m1]
+f_scores_m1 = [metrics_for_thres['F1 Score'] for metrics_for_thres in metrics_m1]
+mcc_m1 = [metrics_for_thres['MCC'] for metrics_for_thres in metrics_m1]
+balanced_acc_m1 = [metrics_for_thres['Balanced Accuracy'] for metrics_for_thres in metrics_m1]
+youden_j_m1 = [metrics_for_thres["Youden's J statistics"] for metrics_for_thres in metrics_m1]
+
 
 plt.plot(x_values, accuracy_m1, color=colors[0], label='Accuracy', alpha=1)
 plt.plot(x_values, precision_m1, color=colors[1], label='Precision')
@@ -304,7 +215,7 @@ max_youden_j_m1_value = max(youden_j_m1)
 max_youden_j_m1_threshold = x_values[youden_j_m1.index(max_youden_j_m1_value)]
 plt.scatter(max_youden_j_m1_threshold, max_youden_j_m1_value, marker='o', color=colors[6])
 
-m1_optimal_thrasholds = {
+m1_optimal_thresholds = {
     'Accuracy': max_accuracy_m1_threshold,
     'Precision': max_precision_m1_threshold,
     'Recall': max_recall_m1_threshold,
@@ -329,6 +240,14 @@ plt.figure(figsize=(10, 6))
 ticks = get_x_values(0, 0.1)
 plt.xticks(ticks)
 plt.yticks(ticks)
+
+accuracy_m2 = [metrics_for_thres['Accuracy'] for metrics_for_thres in metrics_m2]
+precision_m2 = [metrics_for_thres['Precision'] for metrics_for_thres in metrics_m2]
+recall_m2 = [metrics_for_thres['Recall'] for metrics_for_thres in metrics_m2]
+f_scores_m2 = [metrics_for_thres['F1 Score'] for metrics_for_thres in metrics_m2]
+mcc_m2 = [metrics_for_thres['MCC'] for metrics_for_thres in metrics_m2]
+balanced_acc_m2 = [metrics_for_thres['Balanced Accuracy'] for metrics_for_thres in metrics_m2]
+youden_j_m2 = [metrics_for_thres["Youden's J statistics"] for metrics_for_thres in metrics_m2]
 
 plt.plot(x_values, accuracy_m2, color=colors[0], label='Accuracy', alpha=1)
 plt.plot(x_values, precision_m2, color=colors[1], label='Precision')
@@ -386,6 +305,7 @@ plt.savefig('metrics_plot_model2.png', dpi=300)
 plt.show()
 
 # 3rd. task pt. c
+print("\nc. Графіки. Object Counts vs Classifier Score for Model 1/2.")
 
 fig, ax = plt.subplots(figsize=(10, 6))
 
@@ -400,7 +320,7 @@ ax.set_title('Object Counts vs Classifier Score for Model 1')
 ax.set_xlabel('Classifier Score')
 ax.set_ylabel('Object Count')
 
-for el, color in zip(m1_optimal_thrasholds.items(), colors):
+for el, color in zip(m1_optimal_thresholds.items(), colors):
     ax.axvline(x=el[1], linestyle='--', label=el[0], color=color)
 
 ax.legend()
@@ -431,16 +351,17 @@ plt.show()
 
 # 3rd. task pt. d
 
-# RV
+print('''
+d. Збудувати для кожного класифікатору PR-криву та ROC-криву,
+показавши графічно на них значення оптимального порогу. (PR/ROC curve)''')
 
-print(len(recall_for_curve_m1))
-print(len(precision_for_curve_m1))
-print(len(thresholds_pr_m1))
+# RV
 
 # Building graphs
 plt.plot(recall_for_curve_m1, precision_for_curve_m1, label='Model_1_1')
-ideal_points_m1 = np.ones(len(recall_for_curve_m1))  # Создаем массив единиц такой же длины, как и recall_for_curve_m1
-optimal_idx_m1 = np.argmin(np.sqrt((recall_for_curve_m1 - ideal_points_m1) ** 2 + (precision_for_curve_m1 - ideal_points_m1) ** 2))
+ideal_points_m1 = np.ones(len(recall_for_curve_m1))
+optimal_idx_m1 = np.argmin(
+    np.sqrt((recall_for_curve_m1 - ideal_points_m1) ** 2 + (precision_for_curve_m1 - ideal_points_m1) ** 2))
 optimal_threshold_m1 = thresholds_pr_m1[optimal_idx_m1]
 plt.scatter(recall_for_curve_m1[optimal_idx_m1],
             precision_for_curve_m1[optimal_idx_m1])
@@ -448,14 +369,10 @@ plt.annotate(round(optimal_threshold_m1, 2),
              (recall_for_curve_m1[optimal_idx_m1],
               precision_for_curve_m1[optimal_idx_m1]))
 
-
-print(len(recall_for_curve_m2))
-print(len(precision_for_curve_m2))
-print(len(thresholds_pr_m2))
-
 plt.plot(recall_for_curve_m2, precision_for_curve_m2, label='Model_2_1')
-ideal_points_m2 = np.ones(len(recall_for_curve_m2))  # Создаем массив единиц такой же длины, как и recall_for_curve_m2
-optimal_idx_m2 = np.argmin(np.sqrt((recall_for_curve_m2 - ideal_points_m2) ** 2 + (precision_for_curve_m2 - ideal_points_m2) ** 2))
+ideal_points_m2 = np.ones(len(recall_for_curve_m2))
+optimal_idx_m2 = np.argmin(
+    np.sqrt((recall_for_curve_m2 - ideal_points_m2) ** 2 + (precision_for_curve_m2 - ideal_points_m2) ** 2))
 optimal_threshold_m2 = thresholds_pr_m2[optimal_idx_m2]
 plt.scatter(recall_for_curve_m2[optimal_idx_m2],
             precision_for_curve_m2[optimal_idx_m2])
@@ -463,12 +380,12 @@ plt.annotate(round(optimal_threshold_m2, 2),
              (recall_for_curve_m2[optimal_idx_m2],
               precision_for_curve_m2[optimal_idx_m2]))
 
+plt.title('Precision-Recall curve')
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.legend()
-plt.savefig('RV-curves.png', dpi=300)
+plt.savefig('PR-curves.png', dpi=300)
 plt.show()
-
 
 # ROC
 plt.plot(fpr_m1, tpr_m1, label='Model_1_1')
@@ -482,7 +399,6 @@ plt.annotate(round(optimal_threshold_m1, 2),
              (fpr_m1[optimal_idx_m1],
               tpr_m1[optimal_idx_m1]))
 
-
 plt.plot(fpr_m2, tpr_m2, label='Model_1_1')
 ideal_x_m2 = np.zeros(len(fpr_m2))
 ideal_y_m2 = np.ones(len(fpr_m2))
@@ -494,7 +410,6 @@ plt.annotate(round(optimal_threshold_m2, 2),
              (fpr_m2[optimal_idx_m2],
               tpr_m2[optimal_idx_m2]))
 
-
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC-curve')
@@ -502,3 +417,16 @@ plt.legend()
 plt.grid(True)
 plt.savefig('ROC-curves.png', dpi=300)
 plt.show()
+
+
+# Task 4
+print("\n4. Перша модель є кращою, це можна прослідкувати на графіків пунктів 3с. і 3d.\n")
+
+# Task 5
+print('''
+5. Створити новий набір даних, прибравши з початкового набору (50 +
+5К)% об’єктів класу 1, вибраних випадковим чином. Параметр К
+представляє собою залишок від ділення дня народження студента на дев’ять
+та має визначатися в програмі на основі дати народження студента, яка
+задана в програмі у вигляді текстової змінної формату ‘DD-MM’.
+''')
---
+++ lab4/ML_lab4_Lazebnyi_KM_12.py
@@ -0,0 +1,226 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+import seaborn as sns
+
+
+def get_metrics_for_pred(y_pred, y_actual, to_print=True):
+    confusion_matrix_test = metrics.confusion_matrix(y_actual, y_pred)
+
+    accuracy = metrics.accuracy_score(y_actual, y_pred)
+    precision = metrics.precision_score(y_actual, y_pred, average='weighted')
+    recall = metrics.recall_score(y_actual, y_pred, average='weighted')
+    f1_score = metrics.f1_score(y_actual, y_pred, average='weighted')
+    mcc = metrics.matthews_corrcoef(y_actual, y_pred)
+    balanced_accuracy = metrics.balanced_accuracy_score(y_actual, y_pred)
+
+    if to_print:
+        print("Confusion Matrix:\n", confusion_matrix_test)
+        print("Accuracy:", accuracy)
+        print("Precision:", precision)
+        print("Recall:", recall)
+        print("F1 Score:", f1_score)
+        print("MCC: ", mcc)
+        print("Balanced accuracy: ", balanced_accuracy)
+
+    return {
+        "Accuracy": accuracy,
+        "Precision": precision,
+        "Recall": recall,
+        "F1 Score": f1_score,
+        "MCC": mcc,
+        "Balanced Accuracy": balanced_accuracy,
+        "Confusion Matrix": list(confusion_matrix_test)
+    }
+
+
+# Task 1.
+print("\n\n1. Відкрити та зчитати наданий файл з даними.")
+df = pd.read_csv('dataset2_l4.txt', header=0)
+print(df.head)
+
+# Task 2.
+num_records, num_fields = df.shape
+print("\n\n2. Визначити та вивести кількість записів та кількість полів у завантаженому наборі даних.")
+print("Кількість записів:", num_records)
+print("Кількість полів у кожному записі:", num_fields)
+
+# Task 3.
+print("\n\n3. Виведення атрибутів набору даних:")
+print("Атрибути набору даних:")
+print(list(df.columns))
+
+# Task 4.
+print("\n\n4. З’ясувати збалансованість набору даних.\n")
+class_distribution = df['Class'].value_counts()
+print(class_distribution)
+
+print("\nНабір даних не є збалансованим.")
+
+# Task 5.
+print("\n\n5. Отримати двадцять варіантів перемішування набору даних \n"
+      "та розділення його на навчальну (тренувальну) та тестову вибірки.\n"
+      "Сформувати начальну та тестові вибірки на основі обраного користувачем варіанту.\n")
+
+n_splits = 20
+
+shuffle_split = ShuffleSplit(n_splits=n_splits, random_state=32)
+
+variants = []
+for train_index, test_index in shuffle_split.split(df):
+    train_set = df.iloc[train_index]
+    test_set = df.iloc[test_index]
+    variants.append((train_set, test_set))
+
+chosen_variant = int(input("Введіть варіант від 1 до 20: "))
+
+train_set, test_set = variants[chosen_variant - 1]
+
+print("\nРозмір навчальної вибірки:", train_set.shape)
+print("Навчальна вибірка: \n", train_set.head)
+
+print("\nРозмір тестової вибірки:", test_set.shape)
+print("Тестова вибірка: \n", test_set.head)
+
+train_features = train_set.iloc[:, :-1]
+train_labels = train_set.iloc[:, -1]
+
+test_features = test_set.iloc[:, :-1]
+test_labels = test_set.iloc[:, -1]
+
+# Task 6
+print("\n\n6. Збудувати класифікаційну модель на основі методу k найближчих\n"
+      "сусідів (кількість сусідів обрати самостійно, вибір аргументувати) та\n"
+      "навчити її на тренувальній вибірці.")
+
+# --------------- Знаходимо k за допомогою крос-валідації ---------------- #
+
+# k_values = [i for i in range(1, 10)]
+# scores = []
+#
+# for k in k_values:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     score = cross_val_score(knn, df.iloc[:, :-1], df.iloc[:, -1])
+#     scores.append(np.mean(score))
+#
+# plt.plot(k_values, scores)
+# plt.xlabel("k value")
+# plt.ylabel("Accuracy")
+# plt.show()
+
+# ----------- Маємо два кандидати k=1 і k=3 ------------------- #
+
+# k = 1
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(train_features, train_labels)
+
+
+# Task 7
+print("\n\n7. Обчислити класифікаційні метрики збудованої моделі для тренувальної"
+      "та тестової вибірки. Представити результати роботи моделі на тестовій"
+      "вибірці графічно.")
+
+y_pred = knn.predict(test_features)
+
+print("\n\nМетрики для моделі 1-го найближчого сусіда для тестової вибірки")
+metrics_k_1_test = get_metrics_for_pred(y_pred, test_labels, True)
+
+y_pred = knn.predict(train_features)
+
+print("\n\nМетрики для моделі 1-го найближчого сусіда для тренувальної вибірки")
+metrics_k_1_train = get_metrics_for_pred(y_pred, train_labels, True)
+
+# k = 3
+#
+# knn = KNeighborsClassifier(n_neighbors=3)
+# knn.fit(train_features, train_labels)
+#
+# test_labels_pred = knn.predict(test_features)
+#
+# print("\n\nМетрики для моделі 3-х найближчих сусідів")
+# metrics_k_2 = get_metrics_for_pred(test_labels_pred, test_labels, True)
+
+
+
+# --------------- Візуалізація Confusion Matrix -------------------- #
+
+class_labels = list(df["Class"].unique())
+
+plt.figure(figsize=(10, 8))
+heatmap_test = sns.heatmap(metrics_k_1_test["Confusion Matrix"], annot=True, fmt='d', cmap='Blues', xticklabels=class_labels,
+                           yticklabels=class_labels)
+
+plt.show()
+# Для тестової вибірки є помилки.
+
+# ------------- Візуалізація метрик --------------- #
+
+
+plt.figure(figsize=(10, 6))
+plt.bar(list(metrics_k_1_test.keys())[:-1], list(metrics_k_1_test.values())[:-1], color='skyblue')
+
+plt.show()
+
+# --------------- Візуалізація Confusion Matrix -------------------- #
+
+plt.figure(figsize=(10, 8))
+heatmap_train = sns.heatmap(metrics_k_1_train["Confusion Matrix"], annot=True, fmt='d', cmap='Blues', xticklabels=class_labels,
+                            yticklabels=class_labels)
+
+plt.show()
+# Для тестової вибірки є помилки.
+
+# ------------- Візуалізація метрик --------------- #
+
+plt.figure(figsize=(10, 6))
+plt.bar(list(metrics_k_1_train.keys())[:-1], list(metrics_k_1_train.values())[:-1], color='skyblue')
+
+plt.show()
+
+# Task 8
+
+print('''
+8. Обрати алгоритм KDTree та з’ясувати вплив розміру листа (від 20 до
+200 з кроком 5) на результати класифікації. Результати представити
+графічно.
+''')
+
+leaf_sizes = range(20, 201, 5)
+knn_metrics = {'leaf_size': list(leaf_sizes), 'accuracy': [], 'precision': [], 'recall': [], 'f1_score': []}
+
+for leaf_size in leaf_sizes:
+    model = KNeighborsClassifier(n_neighbors=1, algorithm='kd_tree', leaf_size=leaf_size)
+
+    model.fit(train_features, train_labels)
+    model_pred = model.predict(test_features)
+
+    model_metrics = get_metrics_for_pred(model_pred, test_labels, False)
+    knn_metrics['accuracy'].append(model_metrics['Accuracy'])
+    knn_metrics['precision'].append(model_metrics['Precision'])
+    knn_metrics['recall'].append(model_metrics['Recall'])
+    knn_metrics['f1_score'].append(model_metrics['F1 Score'])
+
+    # print(f"\n\n{leaf_size}")
+    # print(np.array(model_metrics['Confusion Matrix']))
+
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(knn_metrics['leaf_size'], knn_metrics['accuracy'], label='Accuracy', alpha=0.5)
+plt.plot(knn_metrics['leaf_size'], knn_metrics['precision'], label='Precision', alpha=0.5)
+plt.plot(knn_metrics['leaf_size'], knn_metrics['recall'], label='Recall', alpha=0.5)
+plt.plot(knn_metrics['leaf_size'], knn_metrics['f1_score'], label='F1 Score', alpha=0.5)
+
+plt.title('Metrics vs Leaf Size')
+plt.xlabel('Leaf Size')
+plt.ylabel('Score')
+plt.legend()
+plt.grid(True)
+plt.show()
+
